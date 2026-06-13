0516e90c1e4818b3e878c8e05eb99474719e8780
761271-netizen <761271@pdsb.net>
2026-06-12 23:49:22 -0400

    Update Attack.java

--- Attack.java
+++ Attack.java
@@ -1,7 +1,6 @@
 abstract class Attack extends GameObject {
     protected final boolean isPlayer;
     protected int damage;
-    protected static int cooldown;
     
     public Attack(int damage, boolean isPlayer)
     {
