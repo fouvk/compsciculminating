f065893b70934e2dee051c1a620ca32c6434a699
761271-netizen <761271@pdsb.net>
2026-06-12 23:54:33 -0400

    Update BossFightGame.java

--- BossFightGame.java
+++ BossFightGame.java
@@ -1,11 +1,18 @@
 import javax.swing.*;
 import java.awt.*;
+import java.nio.file.*;
+import java.util.*;
+import java.util.List;
+
 
 public class BossFightGame extends JFrame
 {
     private CardLayout cardLayout;
     private JPanel mainPanel;
     private String currentUser = "";
+    
+    private Login loginPanel;
+    private EndScreenPanel endScreenPanel;
     
     public BossFightGame() {
         setTitle("BossFightGame");
@@ -16,7 +23,12 @@ public class BossFightGame extends JFrame
         cardLayout = new CardLayout();
         mainPanel = new JPanel(cardLayout);
         
-        mainPanel.add(new Login(this), "LOGIN");
+        loginPanel = new Login(this);
+        mainPanel.add(loginPanel, "LOGIN");
+        mainPanel.add(new HowToPanel(this), "HOWTO");
+        
+        endScreenPanel = new EndScreenPanel(this);
+        mainPanel.add(endScreenPanel, "END");
         
         add(mainPanel);
         cardLayout.show(mainPanel,"LOGIN");
@@ -24,14 +36,53 @@ public class BossFightGame extends JFrame
     
     public void startGame(String username) {
         this.currentUser = username;
-        GamePanel gamePanel = new GamePanel();
+        GamePanel gamePanel = new GamePanel(this);
         mainPanel.add(gamePanel, "GAME");
         cardLayout.show(mainPanel,"GAME");
         gamePanel.requestFocusInWindow();
         GamePanel.gameOver = false;
     }
     public void showLogin() {
+        loginPanel.refreshLeaderboard();
         cardLayout.show(mainPanel,"LOGIN");
+        
+    }
+    
+    public void showHowTo() {
+        cardLayout.show(mainPanel,"HOWTO");
+    }
+    
+    public void showEnd(int playerHP, int bossHP, int stage, int mins, int secs) {
+        endScreenPanel.updateStats(playerHP, bossHP, stage, mins, secs);
+        cardLayout.show(mainPanel, "END");
+    }
+    
+    public List<String> getLeaderboard() {
+        try {
+            Path path = Paths.get("Leaderboard.txt");
+            if (!Files.exists(path)) {
+                return new ArrayList<>();
+            }
+            List<String> lines = Files.readAllLines(path);
+            Collections.sort(lines);
+            return lines;
+        } catch (Exception e) {
+            return new ArrayList<>();
+        }
+    }
+    
+    public void saveScore(String name, int totalSeconds) {
+        List<String> scores = getLeaderboard();
+        scores.add(String.format("%05d,%s", totalSeconds, name));
+        Collections.sort(scores);
+        if (scores.size() > 5) {
+            scores = scores.subList(0,5);
+        }
+        try {
+            Files.write(Paths.get("Leaderboard.txt"), scores);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
     
     public String getCurrentUser()
