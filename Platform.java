71898fa664d4969cebad71b67e77edc872936caf
761271-netizen <761271@pdsb.net>
2026-06-12 23:57:49 -0400

    Update Platform.java

--- Platform.java
+++ Platform.java
@@ -1,17 +1,31 @@
 import java.awt.*;
 import java.util.List;
+import javax.imageio.ImageIO;
+import java.io.IOException;
 class Platform extends GameObject
 {
+    private Image platform;
+    
     public Platform(int x, int y)
     {
         super(x, y, 150, 50);
+        loadPlatform();
+    }
+    
+    public void loadPlatform () {
+        try {
+            platform = ImageIO.read(getClass().getResource("/Images/Platform.png"));
+            }
+        catch (IOException e) {
+            System.out.println("Could not find image");
+            }
     }
     
     @Override
     public void draw(Graphics g)
     {
-        g.setColor(Color.GRAY);
-        g.fillRect((int)x, (int)y, width, height);
+        g.drawImage(platform, (int)x, (int)y - 10, (int)width, (int)height, null);
+            
     }
     
     @Override
